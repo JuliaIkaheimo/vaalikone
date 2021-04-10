ac486b90591ec2d9658ac3b359fd0f2107353c52
almaleino <leino.alma@gmail.com>
2021-04-10 13:49:27 +0300

    psst

---
+++ GH_vaalikone/src/src/main/java/app/Lisaa.java
@@ -0,0 +1,48 @@
+package app;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.Dao;
+import data.Ehdokkaat;
+
+@WebServlet(
+    name = "Lisaa",
+    urlPatterns = {"/lisaa"}
+)
+public class Lisaa extends HttpServlet {
+	private Dao dao;
+	public void init() {
+		dao=new Dao("jdbc:mysql://localhost:3306/vaalikone", "antero", "kukkuu");
+	}
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+	     throws IOException {
+		response.sendRedirect("index.html");
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+	     throws IOException, ServletException {
+		String ehdokas_id=request.getParameter("ehdokas_id");
+		String etunimi=request.getParameter("etunimi");
+		String sukunimi=request.getParameter("sukunimi");
+		String puolue=request.getParameter("puolue");
+
+		Ehdokkaat e=new Ehdokkaat(ehdokas_id, etunimi, sukunimi, puolue);
+
+		ArrayList<Ehdokkaat> list=null;
+		if (dao.getConnection()) {
+			list=dao.lisaaEhdokas(e);
+		}
+
+		request.setAttribute("ehdokkaatlist", list);
+		RequestDispatcher rd=request.getRequestDispatcher("/jsp/showehdokkaat.jsp");
+		rd.forward(request, response);
+	}
+} 
---
+++ GH_vaalikone/src/src/main/webapp/jsp/lisaaehdokkaat.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>    
+
+<%@ include file="../html/header.html" %>
+<div class="container">
+<div class="row">
+<div class="col-9">
+
+<form action='lisaa' method='post'> 
+Ehdokas id: <input type='text' name='ehdokas_id' value=''><br> 
+Ehdokkaan etunimi: <input type='text' name='etunimi' value=''><br>
+Ehdokkaan sukunimi: <input type='text' name='sukunimi' value=''><br>
+Ehdokkaan puolue: <input type='text' name='puolue' value=''><br>
+<input type='submit' name='ok' value='Send'> 
+</form>
+</div>
+</div>
+</div>
+<%@ include file="../html/footer.html" %>
